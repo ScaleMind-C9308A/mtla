48db8d6463b0ca72988701995dcbb730267c39e0
KhoiDOO <khoido8899@gmail.com>
2023-11-21 23:46:25 +0700

    update ds for cityscape

---
+++ dataset/cityscape.py
@@ -0,0 +1,144 @@
+import os, sys
+from typing import *
+import cv2
+from PIL import Image
+from tqdm import tqdm
+
+from torch.utils.data import Dataset, DataLoader
+import torchvision as tv
+import torch.nn.functional as F
+from torch import Tensor
+from torchvision import transforms
+
+
+class CustomCityScapeDS(Dataset):
+    def __init__(self, root = None, split = "train", mode = "fine", 
+                 transform = None, sematic_transform = None):
+        self.split = split
+        self.mode = mode
+        self.images = []
+        self.targets = []
+        self.target_transform = {}
+
+        self.semantic_map = {
+            0 : ['unlabeled', 19, 'void'], 
+            1 : ['ego vehicle', 19, 'void'],
+            2 : ['rectification border', 19, 'void'],
+            3 : ['out of roi', 19, 'void'],
+            4 : ['static', 19, 'void'],
+            5 : ['dynamic', 19, 'void'],
+            6 : ['ground', 19, 'void'],
+            7 : ['road', 0, 'flat'],
+            8 : ['sidewalk', 1, 'flat'],
+            9 : ['parking', 19, 'flat'],
+            10 : ['rail track', 19, 'flat'],
+            11 : ['building', 2, 'construction'],
+            12 : ['wall', 3, 'construction'],
+            13 : ['fence', 4, 'construction'],
+            14 : ['guard rail', 19, 'construction'],
+            15 : ['bridge', 19, 'construction'],
+            16 : ['tunnel', 19, 'construction'],
+            17 : ['pole', 5, 'object'],
+            18 : ['polegroup', 19, 'object'],
+            19 : ['traffic light', 6, 'object'],
+            20 : ['traffic sign', 7, 'object'],
+            21 : ['vegetation', 8, 'nature'],
+            22 : ['terrain', 9, 'nature'],
+            23 : ['sky', 10, 'sky'],
+            24 : ['person', 11, 'human'],
+            25 : ['rider', 12, 'human'],
+            26 : ['car', 13, 'vehicle'],
+            27 : ['truck', 14, 'vehicle'],
+            28 : ['bus', 15, 'vehicle'],
+            29 : ['caravan', 19, 'vehicle'],
+            30 : ['trailer', 19, 'vehicle'],
+            31 : ['train', 16, 'vehicle'],
+            32 : ['motorcycle', 17, 'vehicle'],
+            33 : ['bicycle', 18, 'vehicle'],
+            34 : ['license plate', -1, 'vehicle']
+        }
+        
+        if transform:
+            self.transform = transform
+        else:
+            self.transform = transforms.Compose([transforms.Resize((256, 512)), transforms.ToTensor()])
+        
+        if sematic_transform:
+            self.target_transform['semantic'] = sematic_transform
+        else:
+            self.target_transform['semantic'] = transforms.Compose(
+                [transforms.Resize((256, 512)), transforms.PILToTensor(), self.make_semantic_class])
+
+        self.root = "/media/mountHDD2/cityscapes" if not root else root
+
+        if self.mode == "fine":
+            if self.split not in ['train', 'val', 'test']:
+                raise ValueError("self.split has to be one of ['train', 'val', 'test']")
+        elif self.mode == "coarse":
+            if self.split not in ['train', 'train_extra', 'val']:
+                raise ValueError("self.split has to be one of ['train', 'train_extra', 'val']")
+        else:
+            raise Value_colorError(f"mode cannot be {self.mode}")
+
+        self.img_dir = self.root + f"/leftImg8bit/{self.split}"
+        self.mode_folder = "gtFine" if self.mode == "fine" else "gtCoarse"
+        self.semantic_dir = self.root + f"/{self.mode_folder}/{self.split}"
+        self.depth_dir = self.root + f"/disparity/{self.split}"
+
+        for check_dir in [self.img_dir, self.semantic_dir, self.depth_dir]:
+            if not os.path.exists(check_dir):
+                print(f"The dataset source is not available or not extracted at {self.root}")
+
+        print("Data Set Setting Up")
+        for city in tqdm(os.listdir(self.img_dir)):
+            img_dir = os.path.join(self.img_dir, city)
+            semantic_target_dir = os.path.join(self.semantic_dir, city)
+            depth_target_dir = os.path.join(self.depth_dir, city)
+            for file_name in os.listdir(img_dir):
+                self.images.append(os.path.join(img_dir, file_name))
+
+                fn = file_name.split("_leftImg8bit")[0]
+                mode_folder = semantic_target_dir.split("/")[-3]
+
+                self.targets.append(
+                    {
+                        "semantic" : os.path.join(semantic_target_dir, f"{fn}_{mode_folder}_labelIds.png"),
+                        "depth" : os.path.join(depth_target_dir, f"{fn}_disparity.png"),
+                    }
+                )
+                
+        print("Done")
+    
+    def make_semantic_class(self, x):
+        encx = torch.zeros(x.shape, dtype=torch.long)
+        for label in self.semantic_map:
+            encx[x == label] = self.semantic_map[label][1]
+        print(encx.shape)
+        onehot = F.one_hot(encx.squeeze(1), 20).permute(0, 3, 1, 2)[0].float()
+        return onehot[:-1]
+
+    @staticmethod
+    def process_depth(x):
+        depth = cv2.imread(x, cv2.IMREAD_UNCHANGED).astype(np.float32)
+        depth = cv2.resize(depth, (512, 256))
+        depth[depth > 0] = (depth[depth > 0] - 1) / 256
+
+        depth[depth == np.inf] = 0
+        depth[depth == np.nan] = 0
+        depth[depth < 0] = 0
+
+        torch_depth = torch.from_numpy(depth).unsqueeze(0) / 255.0
+
+        return torch_depth
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img = self.transform(Image.open(self.images[idx]).convert("RGB"))
+
+        target = {}
+        target['semantic'] = self.target_transform['semantic'](Image.open(self.targets[idx]['semantic']))
+        target['depth'] = self.process_depth(self.targets[idx]['depth'])
+
+        return img, target
