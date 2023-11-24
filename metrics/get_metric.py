a3af11eceec0b57e4cb8c6b9ed31fe1916c56913
KhoiDOO <khoido8899@gmail.com>
2023-11-24 15:31:30 +0700

    update metrics dict

--- metrics/get_metric.py
+++ metrics/get_metric.py
@@ -1,4 +1,17 @@
 import os, sys
 
-from .depth import depth_error
-from .seg import 
+from .depth import depth_error as __depth_error
+from .seg import miou as __miou
+from .seg import pixel_accuracy as __pixel_accuracy
+from .acc import accuracy as __accuracy
+
+
+metric_dict = {
+    "depth" : __depth_error,
+    "semantic_miou" : __miou,
+    "semantic_pixel_accuracy" : __pixel_accuracy,
+    "category_accuracy" : __accuracy,
+    "attr_accuracy" : __accuracy
+}
+
+# TODO: implement normal metrics
