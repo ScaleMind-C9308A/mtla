ea74a7b933ea271103a6019b81c9612fe25b635f
KhoiDOO <khoido8899@gmail.com>
2023-11-24 15:16:10 +0700

    udpate pixel accuracy metrics

---
+++ metrics/seg.py
@@ -0,0 +1,32 @@
+import os, sys
+import torch
+import torch.nn.functional as F
+import numpy as np
+
+def miou(pred_mask, mask, smooth=1e-10, n_classes=3):
+    with torch.no_grad():
+        pred_mask = F.softmax(pred_mask, dim=1)
+        pred_mask = torch.argmax(pred_mask, dim=1)
+        pred_mask = pred_mask.contiguous().view(-1)
+        mask = torch.argmax(mask, dim=1)
+        mask = mask.contiguous().view(-1)
+
+        iou_per_class = []
+        for clas in range(0, n_classes): #loop per pixel class
+            true_class = pred_mask == clas
+            true_label = mask == clas
+
+            if true_label.long().sum().item() == 0: #no exist label in this loop
+                iou_per_class.append(np.nan)
+            else:
+                intersect = torch.logical_and(true_class, true_label).sum().float().item()
+                union = torch.logical_or(true_class, true_label).sum().float().item()
+
+                iou = (intersect + smooth) / (union + smooth)
+                iou_per_class.append(iou)
+        return np.nanmean(iou_per_class)
+
+def pixel_accuracy(pred_mask, label_mask):
+    _, _, H, W = label_mask.shape
+
+    return (pred_mask.argmax(dim=1) == label_mask.argmax(dim=1)).sum().item() / (H * W)
