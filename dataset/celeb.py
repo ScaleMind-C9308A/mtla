cca379f70d71a5fea4c41b11229947cafd50c0c3
KhoiDOO <khoido8899@gmail.com>
2023-11-24 15:07:02 +0700

    update celeb ds

---
+++ dataset/celeb.py
@@ -0,0 +1,41 @@
+import os, sys
+from torchvision import transforms
+from torchvision.datasets import CelebA
+
+
+transform = transforms.Compose(
+    [
+        transforms.RandomHorizontalFlip(),
+        transforms.CenterCrop(148),
+        transforms.Resize(64),
+        transforms.ToTensor()
+    ]
+)
+
+
+class CustomCeleb(CelebA):
+    def __init__(self, root="~/data", split='train', download=True, transform=transform):
+        super().__init__(root=root_dir, split=split, download=download, transform=transform, target_type = ['attr', 'identity'])
+        self.attr_names = self.attr_names[:-1]
+
+    def __len__(self):
+        return super().__len__()
+
+    def __getitem__(self, idx):
+        img, target = super().__getitem__(idx)
+
+        attr = target[0]
+        identity = target[1]
+
+        target = {
+            "rec" : img,
+            "identity" : identity
+        }
+
+        attr_dict = {
+            f"attr_{self.attr_names[idx]}" : attr[idx] for idx in range(attr.shape[0])
+        }
+
+        target.update(attr_dict)
+
+        return img, target
