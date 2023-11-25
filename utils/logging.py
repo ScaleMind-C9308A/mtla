69b6f9155f940405fd3c815a49757ee597609761
KhoiDOO <khoido8899@gmail.com>
2023-11-26 00:31:17 +0700

    wandb and board in logging

--- utils/logging.py
+++ utils/logging.py
@@ -1,8 +1,11 @@
 import os, sys
+import time
+
+import wandb
+from torch.utils.tensorboard import SummaryWriter
 
 class Logging:
     def __init__(self, args):
-        self.args = args
         self.__log = {}
         self.__cnt = {
             'train' : 0,
@@ -10,9 +13,25 @@ class Logging:
         }
         self.__epoch = 0
 
-        if self.args.wandb
+        if args.wandb:
+            args.run_name = f"{args.ds}__{args.method}__{int(time.time())}"
 
-        if self.args.log:
+            wandb.init(
+                project=args.wandb_prj,
+                entity=args.wandb_entity,
+                config=args,
+                name=args.run_name,
+                force=True
+            )
+
+        if args.log:
+            self.writer = SummaryWriter(args.exp_dir)
+            self.writer.add_text(
+                "hyperparameters",
+                "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
+            )
+        
+        self.__args = args
 
     
     def __call__(self, key, value, mode='train'):
@@ -22,13 +41,15 @@ class Logging:
             self.__log[key] = value
         self.__cnt[mode] += 1
     
-    def update_wandb(self):
-        pass
+    def __update_wandb(self):
+        for log_key in self.__log_avg:
+            wandb.log({log_key: self.__log_avg[log_key]}, step=self.__epoch)
     
-    def update_board(self):
-        pass 
+    def __update_board(self):
+        for log_key in self.__log_avg:
+            self.writer.add_scalar(log_key, self.__log_avg[log_key], self.__epoch)
     
-    def reset_epoch(self):
+    def __reset_epoch(self):
         self.__log = {}
         self.__cnt = {
             'train' : 0,
@@ -36,11 +57,19 @@ class Logging:
         }
     
     def reset(self):
-        self.reset_epoch()
+        self.__reset_epoch()
         self.__epoch = 0
     
     def step(self):
-        self.reset_epoch()
+        self.__average()
+
+        if self.__args.wandb:
+            self.__update_wandb()
+        
+        if self.__args.log:
+            self.__update_board()
+
+        self.__reset_epoch()
         self.__epoch += 1
     
     @property
@@ -54,3 +83,8 @@ class Logging:
     @property
     def epoch(self):
         return self._Logging__epoch
+
+    def __average(self):
+        self.__log_avg = {
+            log_key : self.__log[log_key] / self.__cnt[log_key.split("/")[0]] for log_key in self.__log
+        }
