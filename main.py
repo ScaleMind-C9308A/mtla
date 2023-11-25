84fe737685bd74fd3d090e3e7aa5e2e9c356f296
KhoiDOO <khoido8899@gmail.com>
2023-11-25 23:54:55 +0700

    update training structure

---
+++ main.py
@@ -0,0 +1,48 @@
+import os, sys
+import argparse
+from typing import *
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog='MTLA')
+
+    # DATASET
+    parser.add_argument('--ds', type=str, required=True, choices = [
+        'oxford', 'nyu', 'celeb', 'city'],
+        help='dataset used in training')
+    parser.add_argument('--bs', type=int, required=True, default=64,
+        help='batch size used for data set')
+    parser.add_argument('--pinmem', action='store_true',
+        help='toggle to pin memory in data loader')
+    parser.add_argument('--wk', type=int, default=12,  
+        help='number of worker processor contributing to data preprocessing')
+    parser.add_argument('--citi_mode', type=str, default='fine',  choices=['fine', 'coarse'],
+        help='mode used for cityscape dataset')
+    
+    # TRAINING GENERAL SETTINGS
+    parser.add_argument('--idx', type=int, default=0,
+        help='device index used in training')
+    parser.add_argument('--method', type=str, default='vanilla', choices=['vanilla'],
+        help='mtl method used in training')
+    parser.add_argument('--epochs', type=int, default=100,
+        help='number of epochs used in training')
+    parser.add_argument('--wandb', action='store_true',
+        help='toggle to use wandb for online saving')
+    parser.add_argument('--log', action='store_true',
+        help='toggle to use tensorboard for offline saving')
+    parser.add_argument('--test', action='store_true',
+        help='toggle to say that this experiment is just flow testing')
+    
+    # MODEL
+    parser.add_argument('--model', type=str, default='unet', choices=['unet'],
+        help='based architecture used in training')
+    parser.add_argument('--init_ch', type=int, default=32,
+        help='number of kernel in the first ')
+    parser.add_argument('--lr', type=float, default=0.001,
+        help='learning rate')
+    
+
+    args = parser.parse_args()
+
+    from trainer import train_func
+
+    train_func(args)
---
+++ trainer.py
@@ -0,0 +1,126 @@
+import os, sys
+from tqdm import tqdm
+
+import torch
+from torch import nn
+from torch.optim import Adam
+from torch.optim.lr_scheduler import CosineAnnealingLR
+
+from dataset import get_ds
+from metrics import metric_dict
+from loss import loss_dict
+from model import model_dict
+from method import method_dict
+
+from utils import folder_setup, save_cfg, Logging
+
+
+def train_func(args):
+
+    # device setup
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu", index = args.idx)
+
+    # folder setup and save setting
+    exp_dir = folder_setup(args)
+    args.exp_dir = exp_dir
+    save_cfg(args, exp_dir)
+
+    # logging setup
+    log_interface = Logging(args)
+
+    # dataset setup
+    data, args = get_ds(args)
+    train_ds, valid_ds, test_ds, train_dl, valid_dl, test_dl = data
+
+    print(f"Number Training Samples: {len(train_ds)}")
+    print(f"Number Validating Samples: {len(valid_ds)}")
+    print(f"Number Testing Samples: {len(test_ds)}")
+
+    print(f"Number Training Batchs: {len(train_dl)}")
+    print(f"Number Validating Batchs: {len(valid_dl)}")
+    print(f"Number Testing Batchs: {len(test_dl)}")
+
+    # model setup
+    model = model_dict[args.model][args.ds](args).to(device)
+    optimizer = Adam(model.parameters(), lr= 0.001)
+    scheduler = CosineAnnealingLR(optimizer, T_max= len(train_dl)*args.epochs)
+
+    # method setup
+    method = method_dict[args.method](args)
+
+    # training
+    for epoch in range(args.epochs):
+
+        model.train()
+
+        for batch, (img, target) in tqdm(enumerate(train_dl)):
+            img = img.to(device)
+            for task in target:
+                target[task] = target[task].to(device)
+
+            pred_target = model(img)
+            losses = []
+            for task in pred_target:
+                for loss_name in loss_dict:
+                    if task in loss_name:
+                        task_loss = loss_dict[loss_name](pred_target[task], target[task])
+                        losses.append(task_loss)
+
+                        log_key = f"train/{task}_loss"
+                        log_interface(key=log_key, value=task_loss.item(), mode='train')
+
+                for metric in metric_dict:    
+                    if task in metric:
+                        value = metric_dict[metric](pred_target[task], target[task])
+
+                        log_key = f"train/{metric}"
+                        log_interface(key=log_key, value=value, mode='train')
+            
+            optimizer.zero_grad()
+            method.backward(model=model, losses=losses)
+            optimizer.step()
+            
+            if batch == 3:
+                break
+        
+        model.eval()
+        with torch.no_grad():
+            for batch, (img, target) in tqdm(enumerate(valid_dl)):
+                img = img.to(device)
+                for task in target:
+                    target[task] = target[task].to(device)
+                
+                pred_target = model(img)
+
+                for task in pred_target:
+                    for loss_name in loss_dict:
+                        if task in loss_name:
+                            task_loss = loss_dict[loss_name](pred_target[task], target[task])
+
+                            log_key = f"valid/{task}_loss"
+                            log_interface(key=log_key, value=task_loss.item(), mode='valid')
+
+                    for metric in metric_dict:    
+                        if task in metric:
+                            value = metric_dict[metric](pred_target[task], target[task])
+
+                            log_key = f"valid/{metric}"
+                            log_interface(key=log_key, value=value, mode='valid')
+
+                if batch == 1:
+                    break 
+        
+        print(log_interface.log)
+        print(log_interface.cnt)
+        print(log_interface.epoch)
+
+        log_interface.step()
+
+        print(log_interface.log)
+        print(log_interface.cnt)
+        print(log_interface.epoch)
+
+        break
+
+
+    # evaluation
