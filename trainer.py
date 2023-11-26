c33d11767c907bcb95a1a97fbffe961389416eb3
KhoiDOO <khoido8899@gmail.com>
2023-11-26 15:49:27 +0700

    udpate trainer with new logging

--- trainer.py
+++ trainer.py
@@ -7,8 +7,8 @@ from torch.optim import Adam
 from torch.optim.lr_scheduler import CosineAnnealingLR
 
 from dataset import get_ds
-from metrics import metric_dict
-from loss import loss_dict
+from metrics import metric_dict, metric_batch
+from loss import loss_dict, loss_batch
 from model import model_dict
 from method import method_dict
 
@@ -25,9 +25,6 @@ def train_func(args):
     args.exp_dir = exp_dir
     save_cfg(args, exp_dir)
 
-    # logging setup
-    log_interface = Logging(args)
-
     # dataset setup
     data, args = get_ds(args)
     train_ds, valid_ds, test_ds, train_dl, valid_dl, test_dl = data
@@ -39,6 +36,9 @@ def train_func(args):
     print(f"Number Training Batchs: {len(train_dl)}")
     print(f"Number Validating Batchs: {len(valid_dl)}")
     print(f"Number Testing Batchs: {len(test_dl)}")
+
+    # logging setup
+    log_interface = Logging(args)
 
     # model setup
     model = model_dict[args.model][args.ds](args).to(device)
@@ -67,21 +67,21 @@ def train_func(args):
                         losses.append(task_loss)
 
                         log_key = f"train/{task}_loss"
-                        log_interface(key=log_key, value=task_loss.item(), mode='train')
+                        log_interface(key=log_key, value=task_loss.item(), mode='train', batch=loss_batch[loss_name])
 
-                for metric in metric_dict:    
+                for metric in metric_dict:
                     if task in metric:
                         value = metric_dict[metric](pred_target[task], target[task])
 
                         log_key = f"train/{metric}"
-                        log_interface(key=log_key, value=value, mode='train')
+                        log_interface(key=log_key, value=value, mode='train', batch=metric_batch[metric])
             
             optimizer.zero_grad()
             method.backward(model=model, losses=losses)
             optimizer.step()
             
-            # if batch == 3:
-            #     break
+            if batch == 3:
+                break
         
         model.eval()
         with torch.no_grad():
@@ -98,21 +98,21 @@ def train_func(args):
                             task_loss = loss_dict[loss_name](pred_target[task], target[task])
 
                             log_key = f"valid/{task}_loss"
-                            log_interface(key=log_key, value=task_loss.item(), mode='valid')
+                            log_interface(key=log_key, value=task_loss.item(), mode='valid', batch=loss_batch[loss_name])
 
                     for metric in metric_dict:    
                         if task in metric:
                             value = metric_dict[metric](pred_target[task], target[task])
 
                             log_key = f"valid/{metric}"
-                            log_interface(key=log_key, value=value, mode='valid')
+                            log_interface(key=log_key, value=value, mode='valid', batch=metric_batch[metric])
 
-                # if batch == 1:
-                #     break 
+                if batch == 1:
+                    break 
 
         log_interface.step()
 
-        # break
+        break
 
 
     # evaluation
