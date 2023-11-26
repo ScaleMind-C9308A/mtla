315a724597159df4cf92df0b52a1c3f0ae232499
KhoiDOO <khoido8899@gmail.com>
2023-11-26 15:47:35 +0700

    update args

--- dataset/getds.py
+++ dataset/getds.py
@@ -26,6 +26,13 @@ def get_ds_ox(args):
     args.seg_n_classes = 3
     args.cls_n_classes = 37
 
+    args.num_train_sample = len(train_ds)
+    args.num_valid_sample = len(valid_ds)
+    args.num_test_sample = len(test_ds)
+    args.num_train_batch = len(train_dl)
+    args.num_valid_batch = len(valid_dl)
+    args.num_test_batch = len(test_dl)
+
     return (train_ds, valid_ds, test_ds, train_dl, valid_dl, test_dl), args
 
 def get_ds_nyu(args):
@@ -37,6 +44,13 @@ def get_ds_nyu(args):
     valid_dl = DataLoader(valid_ds, batch_size=args.bs, shuffle=True, pin_memory=args.pinmem, num_workers=args.wk)
     test_dl = DataLoader(test_ds, batch_size=args.bs, shuffle=True, pin_memory=args.pinmem, num_workers=args.wk)
 
+    args.num_train_sample = len(train_ds)
+    args.num_valid_sample = len(valid_ds)
+    args.num_test_sample = len(test_ds)
+    args.num_train_batch = len(train_dl)
+    args.num_valid_batch = len(valid_dl)
+    args.num_test_batch = len(test_dl)
+
     return (train_ds, valid_ds, test_ds, train_dl, valid_dl, test_dl), args
 
 def get_ds_celeb(args):
@@ -47,6 +61,13 @@ def get_ds_celeb(args):
     train_dl = DataLoader(train_ds, batch_size=args.bs, shuffle=True, pin_memory=args.pinmem, num_workers=args.wk)
     valid_dl = DataLoader(valid_ds, batch_size=args.bs, shuffle=True, pin_memory=args.pinmem, num_workers=args.wk)
     test_dl = DataLoader(test_ds, batch_size=args.bs, shuffle=True, pin_memory=args.pinmem, num_workers=args.wk)
+
+    args.num_train_sample = len(train_ds)
+    args.num_valid_sample = len(valid_ds)
+    args.num_test_sample = len(test_ds)
+    args.num_train_batch = len(train_dl)
+    args.num_valid_batch = len(valid_dl)
+    args.num_test_batch = len(test_dl)
 
     return (train_ds, valid_ds, test_ds, train_dl, valid_dl, test_dl), args
 
@@ -65,6 +86,13 @@ def get_ds_city(args):
     train_dl = DataLoader(train_ds, batch_size=args.bs, shuffle=True, pin_memory=args.pinmem, num_workers=args.wk)
     valid_dl = DataLoader(valid_ds, batch_size=args.bs, shuffle=True, pin_memory=args.pinmem, num_workers=args.wk)
     test_dl = DataLoader(test_ds, batch_size=args.bs, shuffle=True, pin_memory=args.pinmem, num_workers=args.wk)
+
+    args.num_train_sample = len(train_ds)
+    args.num_valid_sample = len(valid_ds)
+    args.num_test_sample = len(test_ds)
+    args.num_train_batch = len(train_dl)
+    args.num_valid_batch = len(valid_dl)
+    args.num_test_batch = len(test_dl)
 
     return (train_ds, valid_ds, test_ds, train_dl, valid_dl, test_dl), args
 
