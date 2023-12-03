7cdb9d40c7cc6c40276c4ffae9b77c3d02296164
KhoiDOO <khoido8899@gmail.com>
2023-12-03 22:55:45 +0700

    add option for segnetoxford pet dataset

--- model/get_model.py
+++ model/get_model.py
@@ -1,9 +1,13 @@
 import os, sys
 
 from .unet_based import OxFordPetUnet
+from .segnet_based import OxFordPetSegNet
 
 model_dict = {
     "unet" : {
         "oxford" : OxFordPetUnet
-    }   
+    },
+    "segnet" : {
+        "oxford" : OxFordPetSegNet
+    }
 }
